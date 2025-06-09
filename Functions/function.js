c0ade5cbf3a39d9a34dc628cfef3a0205d1585cf
SsaGusta <gustavoliveiragf15@gmail.com>
2025-06-09 09:19:18 -0300

    feat: exemplo de funcao calculando potencia

---
+++ Functions/function.js
@@ -0,0 +1,9 @@
+function calculaPotencia (num, pow) {
+    let resultado = 1;
+    for ( let i = 0; i < pow; i++ ) {
+      resultado =  resultado * num;  
+    }
+    return resultado;
+};
+
+console.log(calculaPotencia(4, 3));
