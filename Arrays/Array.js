6d82b72c4f4099bd151a77676c855492e7de5c9a
SsaGusta <gustavoliveiragf15@gmail.com>
2025-06-12 09:38:53 -0300

    feat: Inicializando Array

---
+++ Arrays/Array.js
@@ -0,0 +1,12 @@
+const array = [1, 2, 3, 4];
+
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+
+array[5] = 10;
+
+delete array[5]
+
+console.log(array);
