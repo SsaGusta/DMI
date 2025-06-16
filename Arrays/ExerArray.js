05c37beefdfa47c76c534b84287f8627f88b5f19
SsaGusta <gustavoliveiragf15@gmail.com>
2025-06-16 10:25:33 -0300

    feat: cloning array and add elements

--- Arrays/ExerArray.js
+++ Arrays/ExerArray.js
@@ -126,3 +126,35 @@ console.log('Remover tarefa\n',novastarefas);
 console.log('\n');
 console.log('--------------------------------------------------------------------------------------------------');
 console.log('\n');
+// Em certo momento, o sistema precisa criar uma cópia da lista de pedidos para simular alterações, 
+// sem modificar o array original. Escreva um programa que:
+
+// Crie um array com alguns pedidos.
+// Faça uma cópia do array.
+// Adicione um novo item apenas na cópia.
+// Exiba os dois arrays para mostrar que o original não foi alterado.
+
+const pedidos = ['camiseta', 'calça', 'tênis'];
+
+const novopedido = pedidos.map((str) => {
+    return str;
+});
+novopedido.push('bone');
+
+console.log('Array original: ',pedidos);
+console.log('Array copiado: ',novopedido);
+
+//Resposta Alura
+// const pedidos = ['camiseta', 'calça', 'tênis'];
+
+// const pedidosCopia = pedidos.slice();
+
+// pedidosCopia.push('boné');
+
+// console.log("Array original:", pedidos);
+// console.log("Array copiado:", pedidosCopia);
+console.log('\n');
+console.log('--------------------------------------------------------------------------------------------------');
+console.log('\n');
+
+
