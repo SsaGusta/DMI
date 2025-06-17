693e5c126970400296f64ad3f4b4d54f3d7c9ea5
SsaGusta <gustavoliveiragf15@gmail.com>
2025-06-17 09:06:08 -0300

    feat: indexOf example

--- Arrays/ExerArray.js
+++ Arrays/ExerArray.js
@@ -168,8 +168,8 @@ console.log('\n');
 const livros = ['Dom Casmurro', 'O Cortiço', 'Capitães da Areia', 'Iracema'];
 const livroProcurado = 'O Cortiço';
 
-if (livros === livroProcurado.indexOf[1]){
-    console.log(`O livro "${livroProcurado}" está disponível.`);    
+if (livros.indexOf(livroProcurado) !== -1) {
+  console.log(`O livro "${livroProcurado}" está disponível.`);
 } else {
-    console.log(`O livro "${livroProcurado}" não está disponível.`);
+  console.log(`O livro "${livroProcurado}" não foi encontrado.`);
 }
