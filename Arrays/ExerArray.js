c97bdd70bdb197bab644d53ecd7ae7a5eb9f1b25
SsaGusta <gustavoliveiragf15@gmail.com>
2025-06-17 11:58:59 -0300

    feat: .map an filter example using objects

--- Arrays/ExerArray.js
+++ Arrays/ExerArray.js
@@ -203,3 +203,18 @@ const descprecos = precos.map((num) => {
 console.log('\n');
 console.log('--------------------------------------------------------------------------------------------------');
 console.log('\n');
+
+const participantes = [
+  { nome: 'Ana', idade: 17 },
+  { nome: 'Bruno', idade: 22 },
+  { nome: 'Carla', idade: 19 },
+  { nome: 'Daniel', idade: 15 },
+  { nome: 'Eduarda', idade: 25 }
+];
+
+const partiFiltrado = participantes.filter(p =>{ 
+    p.idade >= 18 ? console.log(`Acesso liberado para: ${p.nome}`) : false;
+    return p.idade >= 18;
+});
+const aprovados = partiFiltrado.map(p => p.nome);
+console.log('Lista de Aprovados:',partiFiltrado);
