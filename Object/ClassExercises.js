087a1da664676155b4777ed61822870acf1b75ff
Gusta <gustavoliveiragf15@gmail.com>
2025-06-30 15:27:36 -0300

    add: Exercises files

---
+++ Object/ClassExercises.js
@@ -0,0 +1,29 @@
+// Você está desenvolvendo um sistema para uma loja online. Cada produto do
+// catálogo deve ser representado por um objeto com informações de nome, preco e disponivel.
+// Escreva um programa que:
+
+// Crie um objeto chamado produto.
+// O objeto deve conter as propriedades: nome, preco, disponivel.
+// Exiba o objeto no console.
+
+const produto = {
+  nome: "Fone de ouvido",
+  preco: 149.9,
+  disponivel: true,
+};
+console.log(
+  `Nome: ${produto.nome}\nPreço: R$ ${produto.preco}\nDisponível: ${produto.disponivel}`
+);
+
+//Resposta Alura
+// const produto = {
+//   nome: 'Fone de ouvido',
+//   preco: 149.9,
+//   disponivel: true
+// };
+
+// console.log(produto);
+
+console.log(
+  "--------------------------------------------------------------------------------------------------"
+);
